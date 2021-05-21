6192ed4bb9544f0a12b43f3b81fd257be1c626ab
HaszonHegedusEszter <hegedus.eszter85@gmail.com>
2021-05-21 11:14:18 +0200

    refact login

--- nemegyedul/src/app/services/database.service.ts
+++ nemegyedul/src/app/services/database.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  dbSubsciption: Subscription | undefined;
+  loggedInUser: any = new Subject<any>();
 
   constructor(private firestore: AngularFirestore) { }
 
@@ -30,31 +30,4 @@ export class DatabaseService {
   deleteData(collection:string, id:string){
     return this.firestore.collection(collection).doc(id).delete()
   }
-
-  //get user data by auth UID:
-  //(app_user_uid is stored in local storage!)
-  getUserByUID(uid:string, collection:string = "users"){
-    let loggedInUser = {};
-    this.dbSubsciption = this.getData(collection).subscribe(
-      (doc:any) => {
-        doc.forEach((user:any)=>{
-          if(user.userUID){
-            if(user.userUID === uid){
-              loggedInUser = user
-            }
-          }
-        });
-      },
-      (err)=>{
-        console.error(err)
-      },
-      ()=>{
-        this.dbSubsciption.unsubscribe()
-      }
-    );
-    return loggedInUser
-  }
-
-
-
 }
