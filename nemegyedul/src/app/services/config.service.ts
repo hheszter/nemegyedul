d9aebfed76bcec01c038af239bd761af2a5d82d0
godo-gabor <ggodga@gmail.com>
2021-05-24 17:00:15 +0200

    new event

--- nemegyedul/src/app/services/config.service.ts
+++ nemegyedul/src/app/services/config.service.ts
@@ -4,13 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  navigation: {label: string, href: string, role: number}[] = [
-    {label: 'Home', href: '/welcome', role: 0},
-    {label: 'Események', href: '/main', role: 1},
-    {label: 'Új esemény felvétele', href: '/event-form', role: 2},
-    {label: 'Eseményeim', href: '/my-events', role: 1},
-    {label: 'Játékok', href: '/games', role: 1},
-    {label: 'Saját adatok', href: '/profil', role: 1},
+  navigation: { label: string, href: string, role: number }[] = [
+    { label: 'Home', href: '/welcome', role: 0 },
+    { label: 'Események', href: '/main', role: 1 },
+    { label: 'Új esemény felvétele', href: '/event-form', role: 2 },
+    { label: 'Eseményeim', href: '/my-events', role: 1 },
+    { label: 'Játékok', href: '/games', role: 1 },
+    { label: 'Saját adatok', href: '/profil', role: 1 },
   ];
   constructor() { }
 }
