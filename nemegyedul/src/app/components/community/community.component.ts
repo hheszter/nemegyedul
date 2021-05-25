d4cee711b2435704b3d6b4dcfb5510c9db304988
HaszonHegedusEszter <hegedus.eszter85@gmail.com>
2021-05-25 13:11:46 +0200

    set unsubscriptions

--- nemegyedul/src/app/components/community/community.component.ts
+++ nemegyedul/src/app/components/community/community.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/model/user';
 import { DatabaseService } from 'src/app/services/database.service';
 
@@ -9,23 +10,57 @@ import { DatabaseService } from 'src/app/services/database.service';
 })
 export class CommunityComponent implements OnInit {
 
+  dbSubscription: Subscription;
   allUser: Array<any>;
+  me: User;
 
   constructor(private db: DatabaseService) { }
 
   ngOnInit(): void {
-    this.db.getData("users").subscribe(
+    this.dbSubscription = this.db.getData("users").subscribe(
       (data:any)=>{
         this.allUser=data;
         this.allUser.map( user => {
           user.category = Object.keys(user.category).filter( index => user.category[index])
         })
       },
-      (err:any)=>console.error(err)
+      (err:any)=>console.error(err),
+      ()=>this.dbSubscription.unsubscribe()
+    )
+
+    this.dbSubscription = this.db.loggedInUser.subscribe(
+      (data:any)=>{
+        this.me=data;
+      },
+      (err:any)=>console.error(err),
+      ()=>this.dbSubscription.unsubscribe()
     )
   }
 
-  setFriend(user:User){
-    console.log("ismerősöd lett: " + user)
+  setFriend(friend:User){
+    console.log(this.me)
+    console.log(friend)
+    
+    // az objektumban a category-t vissza kell állítani, így NEM JÓÓÓÓÓ >>> category-t máshogy mentse...
+    
+    this.me.friends = this.me.friends || {}
+    this.me.friends.friendRequests = this.me.friends.friendRequests || [];
+    if(this.me.friends.friendRequests.includes(friend.id)){
+      alert("Már ismerősnek jelölted (vagy Ő téged!)")
+    }
+    this.me.friends.friendRequests.push(friend.id);
+    this.db.updateData('users', this.me.id, this.me)
+      .then(() => console.log('Add friend-request'))
+      .catch(err => console.error(err))
+    
+    friend.friends = friend.friends || {};
+    friend.friends.friendRequests = friend.friends.friendRequests || [];
+    friend.friends.friendRequests.push(this.me.id);
+    this.db.updateData('users', friend.id, friend)
+      .then(() => console.log('Add friend-request'))
+      .catch(err => console.error(err))
+
+
+
   }
 }
