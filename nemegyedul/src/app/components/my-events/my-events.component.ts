262f00fe3443fe22b5e4a90dd3ba78a8f539574b
NalhiZsolt <nalhizsolt@gmail.com>
2021-06-02 09:11:40 +0200

    myEvent eseményeinél a feliratkozók listázva

--- nemegyedul/src/app/components/my-events/my-events.component.ts
+++ nemegyedul/src/app/components/my-events/my-events.component.ts
@@ -26,46 +26,64 @@ export class MyEventsComponent implements OnInit {
     myEvents: []
   };
   selectedEvent: any = {};
+  users: any[] = [];
+  filteredUsers: any[] = [];
 
 
   constructor(private dataService: DatabaseService) {
     this.dataService.loggedInUser.subscribe(
-      data => { this.currentUser = data; this.myEventIdArray = data.myEvents},
+      data => { this.currentUser = data; this.myEventIdArray = data.myEvents },
       error => console.error(error)
     )
 
   }
 
   ngOnInit(): void {
-    this.dataService.getData('events').subscribe(
+    this.dataService.getData('users').subscribe(
+      data => this.users = data,
+      error => console.error(error)
+    )
 
+    this.dataService.getData('events').subscribe(
       data => {
         this.eventArray = data;
         this.filterData(this.eventArray, this.currentUser, this.myEventArray)
-
-
       },
       error => console.error(error)
     )
 
 
-    }
-  filterData (eventArray, currentUser, myEventArray) {
 
-    for(let j = 0; j< eventArray.length; j++) {
+  }
+  filterData(eventArray, currentUser, myEventArray) {
+
+    for (let j = 0; j < eventArray.length; j++) {
       //let i= 0
       //for (i= 0; i< currentUser.myEvents.length; i++) {
-        // if(eventArray[j].id === currentUser.myEvents[i]) {
-        if(eventArray[j].userUID === currentUser.userUID) {
-          myEventArray.push(eventArray[j])
-          //console.log(myEventArray)
-        }
+      // if(eventArray[j].id === currentUser.myEvents[i]) {
+      if (eventArray[j].userUID === currentUser.userUID) {
+        myEventArray.push(eventArray[j])
+
+        //console.log(myEventArray)
+      }
       //}
     }
 
 
   }
-
+  filterUsers(event) {
+    this.filteredUsers = [];
+    this.selectedEvent = event;
+    let i = 0;
+        for (i = 0; i< this.users.length; i++) {
+          let k = 0;
+          for (k = 0; k < this.users[i].myEvents.length; i++) {
+            if (this.users[i].myEvents[k] === event.id) {
+              this.filteredUsers.push(this.users[i])
+            }
+          }
+        }
+  }
 
   currentEvent(selectedEvent: any) {
     this.selectedEvent = selectedEvent
