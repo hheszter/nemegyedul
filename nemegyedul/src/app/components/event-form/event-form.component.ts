c0321811e56fdda41701e1f58134734d56eb785f
godo-gabor <ggodga@gmail.com>
2021-06-02 07:55:02 +0200

    datetime shenanigans

--- nemegyedul/src/app/components/event-form/event-form.component.ts
+++ nemegyedul/src/app/components/event-form/event-form.component.ts
@@ -15,16 +15,25 @@ export class EventFormComponent implements OnInit, CanActivate {
   newEventForm: FormGroup;
   categories: any;
 
+  minDate: string;
+
+
   constructor(
     public router: Router,
     private db: DatabaseService
   ) {
-
+    this.dateThingy();
   }
 
-
+  dateThingy() {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    this.minDate = tomorrow.toISOString().substring(0, 16);
+  }
 
   ngOnInit(): void {
+
     this.db.loggedInUser.subscribe(
       data => {
         this.currentUser = data
@@ -34,7 +43,7 @@ export class EventFormComponent implements OnInit, CanActivate {
     this.newEventForm = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.pattern(/^[A-ZÁÉÍÓÖŐÚÜŰa-záéíóöőúüű\.\- ]{4,120}$/)]),
       imageUrl: new FormControl('', [Validators.required, Validators.pattern(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)]),
-      datetime: new FormControl('', [Validators.required]),
+      datetime: new FormControl('', [Validators.required, this.dateValidator.bind(this)]),
       shortDescription: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(80)]),
       description: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(800)]),
       location: new FormControl('', [Validators.required, Validators.pattern(/[A-ZÁÉÍÓÖŐÚÜŰa-záéíóöőúüű]./)]),
@@ -58,11 +67,24 @@ export class EventFormComponent implements OnInit, CanActivate {
     return true;
   }
 
+  dateValidator(datetime: FormControl) {
+    const dateValue = datetime.value;
+
+    if (new Date(this.minDate).getTime() > new Date(dateValue).getTime()) {
+      return { 'InvalidDateError': true };
+    }
+    return null;
+  }
+
   saveEvent() {
     const eventData = this.newEventForm.value;
 
     this.db.saveData('events', eventData);
 
+    this.router.navigate(["/events"]);
+  }
+
+  cancelEventSave() {
     this.router.navigate(["/events"]);
   }
 }
