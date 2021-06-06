37fa96d37261b1d33b1ba2aa62a957f8ede6328c
godo-gabor <ggodga@gmail.com>
2021-06-06 18:28:35 +0200

    basic nought 1

--- nemegyedul/src/app/components/games/games.component.ts
+++ nemegyedul/src/app/components/games/games.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { DatabaseService } from 'src/app/services/database.service';
+import { UsersService } from 'src/app/services/users.service';
+import { AngularFirestore } from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-games',
@@ -9,53 +15,174 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class GamesComponent implements OnInit {
 
+  user: User;
+  dbSubscription: Subscription;
   newGameForm: FormGroup;
+  friendsIds: Array<string>;
+
+  friends: any[] = [];
+
+  gamesArray: any[] = [];
+  friendsGamesArray: any[] = [];
 
   games = [
-    'Sakk',
-    'Amőba'
-  ]
-
-  friends: Array<any> = [
     {
-      name: 'Jucus',
-      imageUrl: 'https://images.unsplash.com/photo-1546458904-143d1674858d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=758&q=80'
-    }
-  ];
-
-  matches: Array<any> = [
-    {
-      id: 'safasfgsgg',
-      game: 'nought',
       name: 'Amőba',
-      datetime: new Date(),
-      imageUrl: '../../../assets/images/nought.jpeg',
-      opponent: 'Jucus'
+      game: 'nought'
     },
     {
-      id: 'dsfsdghhd',
-      game: 'chess',
       name: 'Sakk',
-      datetime: new Date(),
-      imageUrl: '../../../assets/images/chess.jpg',
-      opponent: 'Magdolna'
+      game: 'chess'
     }
   ];
 
+  selectedGame = 'nought';
+
   constructor(
+    private db: DatabaseService,
+    private db2: AngularFirestore,
+    private auth: AuthService,
+    private userService: UsersService,
     private router: Router,
     private route: ActivatedRoute
   ) {
     this.newGameForm = new FormGroup({
       game: new FormControl('', Validators.required),
-      friends: new FormControl('', Validators.required)
+      friend: new FormControl('', Validators.required)
     })
   }
 
   ngOnInit(): void {
+    this.dbSubscription = this.db.loggedInUser.subscribe(
+      (user: any) => {
+        this.user = user;
+        // console.log(this.user);
+      },
+      (err: any) => console.error(err),
+      () => this.dbSubscription.unsubscribe()
+    );
+
+    // Getting the ids of our friends
+    this.db2.collection("users", ref => ref
+      .where("id", "==", this.user.id))
+      .get()
+      .subscribe(user => user.forEach(userData =>
+        this.friendsIds = Object(userData.data()).friends.friendLists
+        // console.log(Object(userData.data()).friends.friendLists)
+      ));
+
+    this.db2.collection("users", ref => ref
+      .where("friends.friendLists", "array-contains", this.user.id))
+      .get()
+      .subscribe(user => user.forEach(userData =>
+        // this.friendsIds = Object(userData.data()).friends.friendLists
+        this.friends.push(Object(userData.data()))
+      ));
+
+    // // Getting our friends, max the first ten
+    // this.db2.collection("users", ref => ref
+    //   .where("id", "in", this.friendsIds.slice(0, 10)))
+    //   .get()
+    //   .subscribe(user => user.forEach(userData =>
+    //     this.friends = Object(userData.data())
+    //   ));
+
+    // Getting our ongoing games
+    this.db2.collection("games", ref => ref
+      .orderBy("game.created", "desc")
+      .where("users", "array-contains", this.user.id)
+      .where("game.hasEnded", "==", false))
+      .get()
+      .subscribe((games) => {
+        games.forEach((doc) => {
+          this.gamesArray.push({ id: doc.id, ...Object(doc.data()) });
+          // console.log(doc.id, " => ", doc.data());
+
+          // console.log('/' + Object(doc.data()).game.game + '/' + doc.id);
+        });
+      });
+
+    //Getting all games, to find our friends' games
+    // This is bad-bad-bad practice, never-ever do this
+    // But fror the time-being this is it
+    // this.db2.collection("games")
+    //   .get()
+    //   .subscribe((games) => {
+    //     games.forEach((doc) => {
+    //       this.friendsGamesArray.push({ id: doc.id, ...Object(doc.data()) });
+    //       console.log('My Games: ', doc.id, " => ", doc.data());
+
+    //       console.log('My Games: ', '/' + Object(doc.data()).game.game + '/' + doc.id);
+    //     });
+    //   });
+
+    // console.log(this.gamesArray);
   }
 
   startNewGame() {
+    const formValues = this.newGameForm.value;
+    const { game, friend } = formValues;
 
+    const otherPlayer = this.friends.find(f => f.id === friend);
+    // console.log("FORM VALUES: ", formValues);
+
+    // console.log("FORM VALUES FRIEND:", otherPlayer);
+
+    const player1 = {
+      name: this.user.name,
+      id: this.user.id,
+      photo: this.user.photo,
+      mark: 'o',
+      hasWon: false
+    };
+    const player2 = {
+      name: otherPlayer.name,
+      id: otherPlayer.id,
+      photo: otherPlayer.photo,
+      mark: 'x',
+      hasWon: false
+    }
+
+    const newGame = {
+      board: new Array(81).fill(' '),
+      game: {
+        created: new Date(),
+        game: 'nought',
+        hasEnded: false,
+        imageUrl: '../../../assets/images/nought.jpeg',
+        isDraw: false,
+        name: 'Amőba'
+      },
+      moves: [],
+      next: {
+        player: 'o'
+      },
+      rules: {
+        length: 5,
+        size: 9
+      },
+      players: {
+        o: player1,
+        x: player2
+      },
+      users: [
+        this.user.id,
+        otherPlayer.id
+      ]
+    };
+
+    // console.log('FORM VALUES NEW GAMES: ', newGame);
+
+    switch (this.selectedGame) {
+      case 'nought':
+        const p = this.db.saveData('games', newGame);
+        p.then(data => this.router.navigateByUrl(`nought/${data.id}`));
+        break;
+      case 'chess':
+
+        break;
+      default: break;
+    }
   }
+
 }
--- nemegyedul/src/app/components/games/noughts-and-crosses/noughts-and-crosses.component.ts
+++ nemegyedul/src/app/components/games/noughts-and-crosses/noughts-and-crosses.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { DatabaseService } from 'src/app/services/database.service';
+import { UsersService } from 'src/app/services/users.service';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-noughts-and-crosses',
@@ -7,26 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoughtsAndCrossesComponent implements OnInit {
 
+  user: User;
+  userId: string;
+  dbSubscription: Subscription;
+  gameId: string = ''
+
   markss = {
     x: '../../../../assets/images/noughts-and-crosses/x.png',
     o: '../../../../assets/images/noughts-and-crosses/o.png',
   };
 
-  currentGame = {
+  currentGame: any = {
     game: {
-      isDraw: false
+      created: new Date(),
+      game: 'nought',
+      name: 'Amőba',
+      isDraw: false,
+      hasEnded: false,
+      imageUrl: '../../../assets/images/nought.jpeg',
     },
     players: {
       o: {
-        name: 'Ferenc',
+        name: '',
+        photo: '',
         mark: 'o',
-        id: '1',
+        id: '',
         hasWon: false
       },
       x: {
-        name: 'Jucus',
+        name: '',
+        photo: '',
         mark: 'x',
-        id: '2',
+        id: '',
         hasWon: false
       }
     },
@@ -41,34 +60,138 @@ export class NoughtsAndCrossesComponent implements OnInit {
     },
     // ' ' -> empty, 'x' -> x, 'o' -> o
     // 2 dimensional array
-    state: []
+    board: new Array(81).fill(' '),
+    moves: []
   };
 
   squares: any;
   board: any;
 
-  constructor() {
-    this.initGame();
+  constructor(
+    private db: DatabaseService,
+    private db2: AngularFirestore,
+    private auth: AuthService,
+    private userService: UsersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    // this.initGame();
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.dbSubscription = this.db.loggedInUser.subscribe(
+      (user: any) => {
+        this.user = user;
+        this.userId = this.user.id;
+      },
+      (err: any) => console.error(err),
+      () => this.dbSubscription.unsubscribe()
+    );
+
+    this.route.paramMap.subscribe(params => {
+      this.gameId = params.get('matchId');
+    });
+
+    const gameRef: AngularFirestoreDocument = this.db2.doc(`games/${this.gameId}`);
+    gameRef.valueChanges().subscribe(game => {
+      console.log(game);
+      const newGame = game
+      const newBoard = [];
+      for (let i = 0; i < 9; i++) {
+        newBoard.push(newGame.board.slice(i * 9, i * 9 + 9))
+      }
+      console.log('NEW BOARD: ', newBoard);
+      newGame.board = newBoard;
+
+      this.currentGame.game = newGame.game;
+      this.currentGame.players = newGame.players;
+      this.currentGame.rules = newGame.rules;
+      this.currentGame.next = newGame.next;
+      this.currentGame.board = newGame.board;
+      this.currentGame.moves = newGame.moves;
+      this.currentGame.users = newGame.users;
+
+      console.log('GAME CHANGE DEETECTED: ', this.currentGame);
+
+      if (!!this.currentGame.moves.length) {
+        const lastMove = this.currentGame.moves[this.currentGame.moves.length - 1]
+        console.log(lastMove);
+        const mark = lastMove[0];
+        const x = +lastMove[2];
+        const y = +lastMove[4];
+
+        this.currentGame.moves.forEach(m => this.makeOtherPlayerMove(this.currentGame, m));
+        // this.makeOtherPlayerMove(this.currentGame, lastMove);
+      }
+    });
+
+    //this.userId = this.user.id;
+    //this.db2.collection('games').doc(matchId).update(this.currentGame);
+    // console.log('FIRESTOREGAME: ', this.fireStoreGame);
+
     this.board = document.getElementById('noughts-board');
     this.board.style.pointerEvents = 'all';
+    this.initGame();
     this.getSquares();
     this.addEventListeners();
   }
 
-  initGame() {
-    const grid = new Array(this.currentGame.rules.size)
-      .fill(' ')
-      .map(() => new Array(this.currentGame.rules.size).fill(' '));
-    this.currentGame.state = grid;
+  updateFirestore(game, db, id) {
+    game.board = game.board.flat();
+    db.collection('games').doc(id).update(game);
   }
 
-  makeMove(game: { state: any[][]; next: { player: string; }; }, move: { x: number; y: number; mark: any; }, checkWinFun: (arg0: any) => void) {
-    game.state[--move.x][--move.y] = move.mark;
+  initGame() {
+    const grid = new Array(9)
+      .fill(' ')
+      .map(() => new Array(9).fill(' '));
+    this.currentGame.board = grid;
+
+  }
+
+  makeOtherPlayerMove(game, move) {
+
+    const x = (+move[2] + 1).toString();
+    const y = (+move[4] + 1).toString();
+
+    const square = document.getElementById(x + '-' + y);
+
+    if (square.children.length === 0) {
+      this.createPiece(square, move[0], this.markss);
+
+      this.checkWin(game);
+
+      const message = document.getElementById('message');
+
+      if (game.players.o.hasWon) {
+        this.board.style.pointerEvents = 'none'
+        message.innerText = 'O Nyert';
+      } else if (game.players.x.hasWon) {
+        this.board.style.pointerEvents = 'none'
+        message.innerText = 'X Nyert';
+      } else if (game.game.isDraw) {
+        this.board.style.pointerEvents = 'none'
+        message.innerText = 'Döntetlen';
+      }
+    }
+  }
+
+  makeMove(game: any, move: any, checkWinFun: any, updateFirestoreFun: any, db: any, id: string) {
+    // console.log('BOARD WHEN MAKING A MOVE: ', game.board);
+
+    if (game.board.length == 81) {
+      const newBoard = [];
+      for (let i = 0; i < 9; i++) {
+        newBoard.push(game.board.slice(i * 9, i * 9 + 9));
+      }
+      game.board = newBoard;
+    }
+
+    game.board[--move.x][--move.y] = move.mark;
+    game.moves.push(`${move.mark}:${move.x}-${move.y}`);
     game.next.player = game.next.player === 'x' ? 'o' : 'x';
     checkWinFun(game);
+    updateFirestoreFun(game, db, id);
   }
 
   checkWin(game) {
@@ -92,14 +215,14 @@ export class NoughtsAndCrossesComponent implements OnInit {
       }
     }
 
-    const rows = game.state.map((e: any[]) => e.reduce((a, c) => a + c, ''));
-    const columns = game.state.map((r: any[], i: string | number) => r.reduce((a, _, j) => a + game.state[j][i], ''));
+    const rows = game.board.map((e: any[]) => e.reduce((a, c) => a + c, ''));
+    const columns = game.board.map((r: any[], i: string | number) => r.reduce((a, _, j) => a + game.board[j][i], ''));
 
-    const primaryDiagonalArray = new Array(2 * game.state.length - 1);
-    calculateDiagonalArray(game.state, primaryDiagonalArray);
+    const primaryDiagonalArray = new Array(2 * game.board.length - 1);
+    calculateDiagonalArray(game.board, primaryDiagonalArray);
     const primaryDiagonal = primaryDiagonalArray.map(r => r.reduce((a: any, c: any) => a + c, ''));
 
-    const grid = JSON.parse(JSON.stringify(game.state));
+    const grid = JSON.parse(JSON.stringify(game.board));
 
     function rotateGrid(grid: string | any[]) {
       for (let x = 0; x < grid.length / 2; x++) {
@@ -114,7 +237,7 @@ export class NoughtsAndCrossesComponent implements OnInit {
     }
 
     rotateGrid(grid);
-    const secondaryDiagonalArray = new Array(2 * game.state.length - 1);
+    const secondaryDiagonalArray = new Array(2 * game.board.length - 1);
     calculateDiagonalArray(grid, secondaryDiagonalArray);
     const secondaryDiagonal = secondaryDiagonalArray.map(r => r.reduce((a, c) => a + c, ''));
 
@@ -122,21 +245,24 @@ export class NoughtsAndCrossesComponent implements OnInit {
     [rows, columns, primaryDiagonal, secondaryDiagonal].forEach(arr => arr.forEach(line => {
       if (line.includes(xWinPattern)) {
         game.players.x.hasWon = true;
+        game.game.hasEnded = true;
         return;
       } else if (line.includes(oWinPattern)) {
+        game.game.hasEnded = true;
         game.players.o.hasWon = true;
         return;
       }
     }));
 
-    for (let i = 0; i < game.state.length; i++) {
-      for (let j = 0; j < game.state[0].length; j++) {
-        if (game.state[i][j] === ' ') {
+    for (let i = 0; i < game.board.length; i++) {
+      for (let j = 0; j < game.board[0].length; j++) {
+        if (game.board[i][j] === ' ') {
           return;
         }
       }
     }
     // No more square left to be played
+    game.game.hasEnded = true;
     game.game.isDraw = true;
   }
 
@@ -144,7 +270,7 @@ export class NoughtsAndCrossesComponent implements OnInit {
     this.squares = Array.from(document.querySelectorAll('.square'));
   }
 
-  createPiece(squareElement: HTMLEmbedElement, mark: string, markImage) {
+  createPiece(squareElement: HTMLElement, mark: string, markImage) {
     squareElement.innerHTML = '';
     squareElement.innerHTML = `
     <div class="mark mark-${mark}" data-mark="${mark}" draggable="false">
@@ -159,6 +285,12 @@ export class NoughtsAndCrossesComponent implements OnInit {
     const addMarkFun = this.createPiece;
     const makeMoveFun = this.makeMove;
     const checkWinFun = this.checkWin;
+    const updateFirestoreFun = this.updateFirestore;
+
+    const db = this.db2;
+    const id = this.gameId;
+    const userId = this.userId;
+
     const markImages = this.markss;
 
     const message = document.getElementById('message');
@@ -172,8 +304,18 @@ export class NoughtsAndCrossesComponent implements OnInit {
       const [x, y] = event.target.id.split('-').map(n => +n);
       const mark = state.next.player;
 
-      makeMoveFun(state, { x, y, mark }, checkWinFun);
-      addMarkFun(square, mark, markImages);
+      // console.log('STATE BEFORE MAKING A MOVE: ', state.players[state.next.player].id);
+      // console.log('STATE BEFORE MAKING A MOVE USERID: ', userId);
+
+
+      console.log('STATE BEFORE MAKING A MOVE: ', state);
+
+      if (state.players[state.next.player].id === userId) {
+        console.log('MAKING THE MOVE');
+        makeMoveFun(state, { x, y, mark }, checkWinFun, updateFirestoreFun, db, id);
+        addMarkFun(square, mark, markImages);
+      }
+
 
       if (state.players.o.hasWon) {
         board.style.pointerEvents = 'none'
